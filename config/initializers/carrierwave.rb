e10421933f16984a317122636c7a389b1d745880
p5k6 <p5k6@yahoo.com>
2016-02-23 00:53:58 -0700

    fix up s3 bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,5 +5,5 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     :region                 => ENV['AWS_REGION']
   }
-  config.fog_directory = "labio-wedding"
+  config.fog_directory = "patty-josh-2016-wedding"
 end
